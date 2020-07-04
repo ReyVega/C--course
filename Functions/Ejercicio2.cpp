d0043143c0cd83f3e0e0026b27c0eff2f87588ac
ReyVega <a01114523@itesm.mx>
2020-07-03 23:57:37 -0500

    C++ course

--- Functions/Ejercicio2.cpp
+++ Functions/Ejercicio2.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-void alCuadrado(int dato) {
+template<class E>
+void alCuadrado(E dato) {
     cout << "El cuadrado del numero es: " << dato * dato << endl;
 }
 
 void pedirDatos() {
+    //Cambiar el dato al que gustes
     double dato;
     cout << "Ingresa un numero: ";
     cin >> dato;
